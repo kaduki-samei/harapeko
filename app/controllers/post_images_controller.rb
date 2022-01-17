81fbad8b20f773549e365cb4734a1294ffbc59ba
EC2 Default User <ec2-user@ip-172-31-4-236.ec2.internal>
2022-01-17 01:20:43 +0000

    Up[~index]

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,11 +1,41 @@
 class PostImagesController < ApplicationController
+
   def new
-    @post_image = PostImages.new
+    @post_image = PostImage.new
+  end
+
+  def create
+    @post_image = PostImage.new(post_image_params)
+    @post_image.user_id = current_user.id
+    @post_image.save
+    redirect_to post_images_path
   end
 
   def index
+    @post_images = PostImage.all
   end
 
   def show
   end
+
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  def after_sign_in_path_for(resource)
+    post_images_path
+  end
+  def after_sign_out_path_for(resource)s
+    about_path
+  end
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+
+  private
+
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caption)
+  end
+
 end
